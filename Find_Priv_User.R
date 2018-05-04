3934ca3601635f31b098c20e0047e86779bb28d3
theStache <31371039+theStache@users.noreply.github.com>
2018-05-04 11:13:11 -0400

    Update Find_Priv_User.R
    added better grep expressions and added $ServiceConfidence column

--- Find_Priv_User.R
+++ Find_Priv_User.R
@@ -2,7 +2,7 @@
 #Change the setwd to where your list of files are located
 #Change the inputfiles list to the files named in the directory
 #The outputfile will drop newly created files in the directory specified by setwd
-#Created and last updated by Tyler Williams 20180503
+#Created and last updated by Tyler Williams 20180504.v2
 # :{
 
 old <- Sys.time()
@@ -16,23 +16,36 @@ CSV_read_in <- read.csv(file=inputfile, stringsAsFactors = FALSE)
 #CSV_read_in[is.na(CSV_read_in)] <- 0
 #CSV_read_in <- na.omit(CSV_read_in)
 CSV_read_in$PrivledgedConfidence <- 0
+CSV_read_in$ServiceConfidence <- 0
 new <- Sys.time() - old
 print(new)
 
 x <- grep("svc|service|admin|administrator",CSV_read_in$MemberOf, ignore.case = TRUE)
 for(rn in 1:length(x))CSV_read_in$PrivledgedConfidence[x[rn]] <- 1
 
-y <- grep("svc|service|admin|administrator",CSV_read_in$UserPrincipalName, ignore.case = TRUE)
-for(rn in 1:length(x))CSV_read_in$PrivledgedConfidence[x[rn]] <- 1
+y <- grep("svc|service|admin|administrator|\\.sa|\\.oa|\\.da|\\.ea",CSV_read_in$UserPrincipalName, ignore.case = TRUE)
+for(rn in 1:length(x))CSV_read_in$PrivledgedConfidence[y[rn]] <- 1
 
 z <- (grep("TRUE",CSV_read_in$TrustedForDelegation))
 for(rn in 1:length(z))CSV_read_in$PrivledgedConfidence[z[rn]] <- 1
 
-xx <- (grep("svc|service|admin|administrator",CSV_read_in$SamAccountName, ignore.case = TRUE))
+xx <- (grep("svc|service|admin|administrator|\\.sa|\\.oa|\\.da|\\.ea",CSV_read_in$SamAccountName, ignore.case = TRUE))
 for(rn in 1:length(xx))CSV_read_in$PrivledgedConfidence[xx[rn]] <- 1
 
 yy <- (grep("svc|service|admin|administrator",CSV_read_in$DistinguishedName, ignore.case = TRUE))
 for(rn in 1:length(yy))CSV_read_in$PrivledgedConfidence[yy[rn]] <- 1
+
+a <- grep("svc|service",CSV_read_in$MemberOf, ignore.case = TRUE)
+for(rn in 1:length(x))CSV_read_in$ServiceConfidence[a[rn]] <- 1
+
+b <- grep("svc|service|\\.sa",CSV_read_in$UserPrincipalName, ignore.case = TRUE)
+for(rn in 1:length(x))CSV_read_in$ServiceConfidence[b[rn]] <- 1
+
+d <- (grep("svc|service|\\.sa",CSV_read_in$SamAccountName, ignore.case = TRUE))
+for(rn in 1:length(xx))CSV_read_in$ServiceConfidence[d[rn]] <- 1
+
+e <- (grep("svc|service",CSV_read_in$DistinguishedName, ignore.case = TRUE))
+for(rn in 1:length(yy))CSV_read_in$ServiceConfidence[e[rn]] <- 1
 
 print(Sys.time())
 write.csv(CSV_read_in,outputfile, row.names = FALSE)
